3f0dc277c7d5af8f6fd03ff43f85428b8b6bc81b
Rocio <rocio95cua>
2019-03-25 19:27:02 -0600

    add calculus form sensors

--- software/read_mics4514/read_mics4514.ino
+++ software/read_mics4514/read_mics4514.ino
@@ -12,106 +12,87 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 #define Serial SerialUSB
+#define e 2.71828
 
 //analog read
-int ox=0;
-int red=0;
-int co2=0;
+int co=0;
 int no2=0;
 
 //Value voltaje
-float vox=0;
-float vred=0;
-float vco2=0;
+float vco=0;
 float vno2=0;
 
 //Value resistencia
-float rox=0;
-float rred=0;
-float rco2=0;
+float rco=0;
 float rno2=0;
+
+//Value Rs/Ro
+float conCO=0;
+float conNO2=0;
+
+//cálculo de ppm 
+double ppmCO=0;
+double ppmNO2=0;
 
 void setup() {
   //resolution analog reads
    analogReadResolution(12);
    Serial.begin(9600);
-
   //pre heating
-  pinMode(2,OUTPUT);
+  //pinMode(2,OUTPUT);
   Serial.println("pre heating");
-  digitalWrite(2, HIGH);
-  delay (30000);
+  //digitalWrite(9, HIGH);
+  //delay (30000);
   Serial.println("pre heating done");
-  digitalWrite(2, LOW);
+  //digitalWrite(2, LOW);
 }
 
 void loop() {
   
   //Get Sensor Data
-  ox=analogRead(A0);
-  red=analogRead(A1);
-  co2=analogRead(A2);
-  no2=analogRead(A3);
+  co=analogRead(A0);
+  no2=analogRead(A1);
   
-  Serial.print("bits ox:");
   Serial.print(",");
-  Serial.print(ox);
+  Serial.print(co);
   Serial.print(",");
-  Serial.print(" bits red:");
-  Serial.print(",");
-  Serial.print(red);
-  Serial.print(",");
-  Serial.print(" bits co2:");
-  Serial.print(",");
-  Serial.print(co2);
-  Serial.print(",");
-  Serial.print(" bits no2:");
-  Serial.print(",");
-  Serial.println(no2);
+  Serial.print(no2);
     
   //Convert to voltaje
-  vox=(3.3*ox)/4096;
-  vred=(3.3*red)/4096;
-  vco2=(3.3*co2)/4096;
+  vco=(3.3*co)/4096;
   vno2=(3.3*no2)/4096;
    
-  Serial.print("V ox:");
   Serial.print(",");
-  Serial.print(vox);
+  Serial.print(vco);
   Serial.print(",");
-  Serial.print(" V red:");
-  Serial.print(",");
-  Serial.print(vred);
-  Serial.print(",");
-  Serial.print(" V co2:");
-  Serial.print(",");
-  Serial.print(vco2);
-  Serial.print(",");
-  Serial.print(" V no2:");
-  Serial.print(",");
-  Serial.println(vno2);
+  Serial.print(vno2);
 
   //Convert to resist
-  rox=((3.3*270)/vox)-270; //load resistor in ox 270ohm
-  rred=((3.3-vred)*47000)/vred; //load resistor in red 1ohm
-  rco2=((3.3*47000)/vco2)-47000;
-  rno2=((3.3*270)/vno2)-270;
+  rco=47000*((3.3-vco)/vco);//load resistor in red 1ohm
+  rno2=4270*((3.3-vno2)/vno2);//load resistor in ox 270ohm
+  
+  Serial.print(",");
+  Serial.print(rco);
+  Serial.print(",");
+  Serial.print(rno2);
 
-  Serial.print("R ox:");
+ //Convert to indicator concentration
+  conCO= 47000/rco;
+  conNO2= 270/rno2;
+  
   Serial.print(",");
-  Serial.print(rox);
+  Serial.print(conCO);
   Serial.print(",");
-  Serial.print(" R red:");
+  Serial.print(conNO2);
+
+  //Calculo de particulas por millon 
+  ppmCO=971.43*pow(e,-9177*conCO);
+  ppmNO2= 11.455*pow(conNO2, 0.5399);
+
   Serial.print(",");
-  Serial.print(rred);
+  Serial.print(ppmCO);
   Serial.print(",");
-  Serial.print(" R co2:");
-  Serial.print(",");
-  Serial.println(rco2);
-  Serial.println(",");
-  Serial.print(" R no2:");
-  Serial.print(",");
-  Serial.println(rno2);
-    
+  Serial.println(ppmNO2);
+ 
   delay(1000);
 }
